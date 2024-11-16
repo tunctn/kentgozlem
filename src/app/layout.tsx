05ba5669c245b7bcd19eba575b9f46f30f6e73af
Tunç Türkmen <t@tunc.co>
2024-11-16 15:05:32 +0100

    fix: theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default async function RootLayout({
 	return (
 		<html lang="tr" suppressHydrationWarning>
 			<body className={`${inter.className} antialiased`}>
-				<ThemeProvider attribute="class" forcedTheme={theme}>
+				<ThemeProvider attribute="class" forcedTheme={theme === "dark" ? "dark" : "light"}>
 					<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 					<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
 					<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
